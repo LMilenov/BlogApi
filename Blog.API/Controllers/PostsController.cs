f6c3bbbc334b58603ca9fdebccd13edd53b26db3
Lucy Milenov <lucymilenov@Lucys-MacBook-Air.local>
2025-09-19 10:05:57 +0300

    Added CreatePost method

--- Blog.API/Controllers/PostsController.cs
+++ Blog.API/Controllers/PostsController.cs
@@ -33,7 +33,7 @@ public class PostsController : ControllerBase
             Content = p.Content,
             CreatedAtUtc = p.CreatedAtUtc,
             Tags = p.PostTags.Select(pt => pt.Tag!.Name).ToList(),
-            Comments = p.Comments.Select( c => new CommentReadDto
+            Comments = p.Comments.Select(c => new CommentReadDto
             {
                 Id = c.Id,
                 Author = c.Author,
@@ -49,5 +49,53 @@ public class PostsController : ControllerBase
         }
 
         return Ok(posts);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PostReadDto>> CreatePost(PostCreateDto dto)
+    {
+        if (string.IsNullOrWhiteSpace(dto.Title))
+        {
+            return BadRequest("Title is required");
+        }
+
+        var post = new Post
+        {
+            Title = dto.Title,
+            Content = dto.Content,
+            CreatedAtUtc = DateTime.UtcNow
+        };
+
+        _context.Posts.Add(post);
+        await _context.SaveChangesAsync();
+
+        if (dto.TagIds != null && dto.TagIds.Any())
+        {
+            foreach (var tagId in dto.TagIds)
+            {
+                var tagExists = await _context.Tags.AnyAsync(t => t.Id == tagId);
+                if (!tagExists)
+                {
+                    return BadRequest("Invalid tagId: {tagId}");
+                }
+                _context.PostTags.Add(new PostTag { PostId = post.Id, TagId = tagId });
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        var result = new PostReadDto
+        {
+            Id = post.Id,
+            Title = post.Title,
+            Content = post.Content,
+            CreatedAtUtc = post.CreatedAtUtc,
+            Tags = _context.PostTags
+            .Where(pt => pt.PostId == post.Id)
+            .Select(pt => pt.Tag!.Name)
+            .ToList(),
+            Comments = new List<CommentReadDto>()
+        };
+
+        return CreatedAtAction(nameof(GetPosts), new { id = post.Id }, result);
     }
 }
